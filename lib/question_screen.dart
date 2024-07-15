d09c88429cc69297b11fe1d6e3eafcb1178ea91a
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-15 23:20:05 +0800

    mapping list and using spread operator

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -13,30 +14,25 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The Question...',
-            style: TextStyle(
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Colors.white,
             ),
           ),
           const SizedBox(height: 30),
-          AnswerButton(
-            onTap: () {},
-            answerText: "Answer text....",
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: "Answer text....",
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: "Answer text....",
-          ),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(
+              onTap: () {},
+              answerText: item,
+            );
+          }),
         ],
       ),
     );
